d6936be9fe98687ffa0e25784a3ef7ff17c1553e
dkAsByHa <asmus.byskov.hansen@lego.com>
2022-09-10 06:46:09 +0200

    Extended binary search tree

--- symbol_tables/binary_search_tree.py
+++ symbol_tables/binary_search_tree.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 class Node:
 
@@ -41,9 +42,11 @@ class Node:
 class BinarySearchTree:
 
     root = None
-
+    time_put = 0
+    puts = 0
+    
     def size(self):
-        return self.size(self.root)
+        return self.size_(self.root)
 
     def size_(self, x):
         if x is None:
@@ -52,6 +55,7 @@ class BinarySearchTree:
             return x.count
 
     def get(self, key):
+        #print(f'get - key: {key}')
         return self.get_(self.root, key)
 
     def get_(self, x, key):
@@ -61,32 +65,48 @@ class BinarySearchTree:
 
         cmp = x.compare_to(key)
 
+        #print(f'Node key {x.key}, get key {key}, cmp: {cmp}')
+
         # If x is less than key - Get left
         if cmp < 0:
-            print(f'New key {key} less than node key {x.key} - Get right')
-            self.get_(x.right, key)
+            #print(f'Get right')
+            return self.get_(x.right, key)
         elif cmp > 0:
-            print(f'New key {key} larger than node key {x.key} - Get left')
-            self.get_(x.left, key)
+            #print(f'Get left')
+            return self.get_(x.left, key)
         else:
+            #print(f'Return val: {x.val}')
             return x.val
 
+    def contains(self, key):
+        if self.get(key) is None:
+            return False
+        else:
+            return True
+
     def put(self, key, val):
+        #print(f'put - key: {key}, val: {val}')
+        time_start = time.time()
         self.root = self.put_(self.root, key, val)
+        self.time_put += time.time() - time_start
+        self.puts += 1
+        #print(f'time_put: {self.time_put}, puts: {self.puts}')
 
     def put_(self, x, key, val):
-        print(f'put_ - x: {x}')
+        
         if x is None:
             return Node(key=key, val=val, count=1)
         
         cmp = x.compare_to(key)
 
+        #print(f'Node key {x.key}, insert key {key}, cmp: {cmp}')
+
         # If x is less than key - Put left
         if cmp < 0:
-            print(f'Node key {key} less than new key {x.key} - Put right')
+            #print(f'Put right')
             x.right = self.put_(x.right, key, val)
         elif cmp > 0:
-            print(f'Node key {key} larger than new key {x.key} - Put left')
+            #print(f'Put left')
             x.left = self.put_(x.left, key, val)
         # If keys are equal - Overwrite val
         else:
@@ -112,13 +132,60 @@ class BinarySearchTree:
 
         return self.is_bst_(x.left) and self.is_bst_(x.right)
 
+    def keys(self):
+        return self.keys_(self.root)
 
+    def keys_(self, x):
+
+        keys = [x.key]
+
+        if x.left is not None:
+            keys += self.keys_(x.left)
+
+        if x.right is not None:
+            keys += self.keys_(x.right)
+
+        return keys
+
+    def min(self):
+        return self.min_(self.root)
+
+    def min_(self, x):
+
+        if x.left is None:
+            return x.val
+        else:
+            return self.min_(x.left)
+
+    def max(self):
+        return self.max_(self.root)
+
+    def max_(self, x):
+
+        if x.right is None:
+            return x.val
+        else:
+            return self.max_(x.right)
+
+'''
 bst = BinarySearchTree()
 
-arr = np.array([3, 2, 4, 1, 2.5])
+arr = np.array([3, 2, 4, 1, 2.5, 5])
 
 for val, key in zip(arr, arr):
     print(f'Key: {key}, val: {val}')
 
     bst.put(key, val)
     print(f'BST Valid? {bst.is_bst()}')
+
+arr = np.array([3, 2, 4, 1, 2.5, 0.5, 5])
+
+for key in arr: 
+    val = bst.get(key)
+    print(f'Get - Key: {key}, val: {val}')
+
+print(f'BST Size: {bst.size()}')
+print(f'BST Min: {bst.min()}')
+print(f'BST Max: {bst.max()}')
+print(f'BST Keys: {bst.keys()}')
+'''
--- symbol_tables/frequency_counter.py
+++ symbol_tables/frequency_counter.py
@@ -1,8 +1,11 @@
+from msilib.schema import Binary
 from naive_st import NaiveSymbolTable
+from binary_search_tree import BinarySearchTree
 import time
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+from tqdm import tqdm
 
 def read_file(file_path):
 
@@ -21,48 +24,90 @@ def read_file(file_path):
 
     return words
 
-def frequency_counter(words, st=NaiveSymbolTable):
+def frequency_counter(words, st=None):
 
     symbol_table = st()
 
-    for word in words:
+    max_key = None
+    max_val = 0
+
+    for word in (words):
 
         if symbol_table.contains(word):
-            symbol_table.put(word, symbol_table.get(word)+1)
+            val = symbol_table.get(word)+1
+            symbol_table.put(word, val)
+            
+            if val > max_val:
+                max_val = val
+                max_key = word
+            
         else:
             symbol_table.put(word, 1)
 
+    '''
     # Find key with highest count
     max_word = ''
     symbol_table.put(max_word, 0)
 
-    for key in symbol_table.keys():
+    for key in tqdm(symbol_table.keys()):
         if symbol_table.get(key) > symbol_table.get(max_word):
             max_word = key
-
-    print(f'Max word: {max_word}, count: {symbol_table.get(max_word)}')
+    '''
+    #time_put = symbol_table.time_put
+    #print(f'Max word: {max_key}, count: {max_val}')
+    
+    #return time_put
 
 file_location = r'C:\source/algorithms_and_datastructures/symbol_tables/'
-file_path = 'valiant_tailor.txt'
+file_path = 'leipzig1M.txt'
 #file_path = 'simple_test.txt'
 words = read_file(file_location + file_path)
-random.shuffle(words)
+words = [word for word in words if len(word) > 8]
 
 lens = []
 ts = []
-downsample = [10, 8, 6, 4, 2, 1]
-for d in downsample:
-    
+ts_bst = []
+downsample = [64, 32, 16]
+tests = 100
+
+for t in tqdm(np.arange(tests)):
+    d = np.random.randint(200, 10000)
+    #random.shuffle(words)
+
+    words = words[1:] + words[:1]
+
+    words_downsample = words[:d]
+
+    words_downsample_len = len(words_downsample)
+    #print(f'Len words: {len(words_downsample)}')
+
     time_start = time.time()
-    frequency_counter(words[::d], st=NaiveSymbolTable)
-    ts += [time.time() - time_start]
-    lens +=[len(words[::d])]
+    frequency_counter(words_downsample, st=NaiveSymbolTable)
+    time_stop = time.time()
+    ts += [(time_stop-time_start) * 1000 ]
+
+    time_start = time.time()
+    frequency_counter(words_downsample, st=BinarySearchTree)
+    time_stop = time.time()
+    ts_bst += [(time_stop-time_start) * 1000 ]
+
+    lens +=[words_downsample_len]
 
 lens = np.array(lens)
 ts = np.array(ts)
+ts_bst = np.array(ts_bst)
+
+ts = (ts - np.percentile(ts, q=1)) / (np.percentile(ts, q=99) - np.percentile(ts, q=1))
+ts_bst = (ts_bst - np.percentile(ts_bst, q=1)) / (np.percentile(ts_bst, q=99) - np.percentile(ts_bst, q=1))
+
+print(f'ts: {ts}')
+print(f'ts_bst: {ts_bst}')
 
 plt.figure()
-plt.scatter(lens, ts, alpha=1)
+plt.scatter(lens, ts, alpha=0.5, label='Naive')
+plt.scatter(lens, ts_bst, alpha=0.5, label='Binary search tree')
+plt.legend()
+plt.yscale('log')
 plt.xlabel('N')
 plt.ylabel('Time')
 plt.show()
